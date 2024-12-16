ebab8bdd15c38f5d3c6913b5dcb7ca93b6b64f78
anusreebuyexchange <anusree@buyexchange.in>
2024-12-16 14:26:47 +0000

    Resolve "#8: Created email subscription section"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Hero } from "./components/hero"
+import Email from './components/Email';
 
 const Home = () => {
   return (
     <div>
       <Hero/>
+     <Email/>
     </div>
   );
 };
