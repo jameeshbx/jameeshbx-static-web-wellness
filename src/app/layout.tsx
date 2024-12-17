96552b25a287bdfffdf4a6e37f27ed7b733279fa
AnandBuyExchange <anand@buyexchange.in>
2024-12-17 11:42:58 +0000

    Resolve "FE: Create a footer section"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Red_Hat_Text } from 'next/font/google';
 import './globals.css';
 import Header from "./components/Header"
+import Footer from './components/Footer';
 
 const redHat = Red_Hat_Text({
   subsets: ['latin'],
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body className={redHat.className}>
           <Header/>
           {children}
+          <Footer/>
       </body>
     </html>
   );
