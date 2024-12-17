4c0d247605fade1659cfd194c9c67eaf9bdcbd0e
NishaMol-BuyEx <nishamol@buyexchange.in>
2024-12-17 12:14:26 +0000

    Resolve "#5: Create a gallery section"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Hero } from "./components/hero"
+import Gallery from './components/Gallery';
 import Email from './components/Email';
 import { CategorySection } from './components/categorysection';
 
 const Home = () => {
   return (
     <div>
-      <Hero />
-      <CategorySection />
-      <Email />
+      <Hero/>
+      <CategorySection/>
+      <Gallery/>
+      <Email/>
     </div>
   );
 };
