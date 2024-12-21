a1d4076cc05af215bd1b4850cccbc288f81d5114
anusreebuyexchange <anusree@buyexchange.in>
2024-12-21 00:07:08 +0000

    Resolve "#7: create a testimonial section"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,17 @@ import { Hero } from "./components/hero"
 import { RecommendedSection } from "./components/Recommendedsection"
 import Gallery from './components/Gallery';
 import Email from './components/Email';
+import TestimonialSection from './components/TestimonialSection';
 import { CategorySection } from './components/categorysection';
 import  CookieConsent  from './components/cookies'
+
 
 const Home = () => {
   return (
     <div>
       <CookieConsent/>
       <Hero/>
+     <TestimonialSection/>
       <CategorySection/>
       <Gallery/>      
       <RecommendedSection/>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,6 +24,8 @@ const config: Config = {
         lightblue: "var(--lightblue)",
         info:"var(--info)",
         grey:"var(--grey)",
+        violet:"var(--violet)",
+        purple:"var(--purple)",
         primarytext: "var(--primarytext)",
         pink:"var(--pink)",
         darkpink:"var(--darkpink)",
@@ -36,17 +38,58 @@ const config: Config = {
         playfair: ['"Playfair Display"', 'serif'],
         Playfair:['"Playfair Display"','sans-serif']
       },
-      height: {
-        '98': '435px',
-      },
       spacing: {
         '187': '187px',
         '28': '28px',
+        '128':'128px',
+        '179':'179px',
+        '5':'5px',
+        '96':'96px',
+        '725':'725px',
+        '433':'433px',
+        '14':'14px',
+        '53':'53px',
+        '443':'443px',
+        '167':'167px',
+        '365':'365px',
+        '264':'264px',
+        '324':'324px',
+       '2':'2px',
+        '40':'40px',
+        '458':'458px'
+
       },
-      width: {
+      height:{
+        '98': '435px',
+        '688':'688px',
+        '131':'131vh',
+        '93':'93vh',
+        '443':'443px',
+        '400':'400px',
+        '80':'80px',
+        '100':'100px',
+        '160':'160px',
+        '200':'200px',
+        '212':'212',
+        '280':'280',
+        '360':'360'
+      },
+      width:{
+        '246':'246vh',
+        '1682':'1682px',
+        '40': '40%', 
         '40p': '40%', 
         '900':'900px'
       },
+      letterSpacing: {
+        '-2': '-0.02em', 
+      },
+      borderRadius: {
+        '32': '32px', 
+      },
+      
+        
+      
     },
   },
   plugins: [],
